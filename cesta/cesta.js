e7299970a0b14224557ffd2a5eadf4f59af12f94
ARJIT <arjitbhandari222830@gmail.com>
2019-11-13 03:31:10 +0530

    adding few changes

--- cesta/cesta.js
+++ cesta/cesta.js
@@ -14,10 +14,13 @@ function scroll() {
     if (document.body.scrollTop >= x)
     {
         navbar.style.backgroundImage= "linear-gradient( 112.2deg,  rgba(86,223,223,1) 4.7%, rgba(2,28,186,1) 110.1% )";
-        console.log("hello");
+        navbar.style.opacity= ".95";
     }
     else
+    {
         navbar.style.backgroundImage= "";
+        navbar.style.opacity= "1";
+    }
 }
 
 function resize(){
